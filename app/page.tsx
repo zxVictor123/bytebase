7181135569dee3dd72338415b827856b59b73a45
dzx <dzxdetianxuan@outlook.com>
2025-09-22 23:20:07 +0800

    添加去登陆按钮

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>haha</div>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <Button asChild className="w-20 h-20"><Link href="sign-in">去登录</Link></Button>
+    </div>
   );
 }
